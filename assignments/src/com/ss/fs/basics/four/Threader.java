cdf87f382f69a17719ef1069b6007e2094473844
joshua-davis1 <joshua.davis@smoothstack.com>
2021-10-24 21:45:45 -0400

    replaced primative array wtih a queue

--- assignments/src/com/ss/fs/basics/four/Threader.java
+++ assignments/src/com/ss/fs/basics/four/Threader.java
@@ -1,19 +1,17 @@
 package com.ss.fs.basics.four;
 
+import java.util.PriorityQueue;
+
 public class Threader {
     volatile public static Threader instance = null;
-    private final Integer[] dataStream = new Integer[5];
+    private final PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
 
     private Threader() {
-        System.out.println("Threader has been initiated!");
+        System.out.println("\nThreader has been initiated!");
     }
 
-    public Integer[] getDataStream() {
-        return dataStream;
-    }
-
-    void setPacket(Integer packet, int index) {
-        dataStream[index] = packet;
+    public PriorityQueue<Integer> getQueue() {
+        return queue;
     }
 
     public static Threader getInstance() {
